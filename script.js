2d3cc4f082e9aa53f60d0caf2f1e89fe67effb87
Shubh Raj Singh <singh.shubhraj06@gmail.com>
2025-07-05 23:18:47 +0530

    v3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-meniIcon.onclick = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
@@ -23,10 +23,25 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active section for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeat on scroll use this
+        else {
+            sec.classList.remove('show-animate');
         }
     })
-    // sticky 
+    // sticky header
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer = document.querySelector('footer');
+
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
